4c3c9ec896aaa1f880aefde3c3c9f974d3698468
Jared Kobos <jaredkobos@gmail.com>
2019-12-08 17:05:24 -0500

    Switch to https when posting to elm-api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import './main.css';
-import { Elm } from './Main.elm';
-import registerServiceWorker from './registerServiceWorker';
+import "./main.css";
+import { Elm } from "./Main.elm";
+import registerServiceWorker from "./registerServiceWorker";
 
-var settings = localStorage.getItem('settings');
+var settings = localStorage.getItem("settings");
 
 var app = Elm.Main.init({
-  node: document.getElementById('root'),
+  node: document.getElementById("root"),
   flags: {
     settings: JSON.parse(settings),
-    apiUrl: "http://api.kobonaut.com/feedback" // process.env.ELM_APP_API_URL
+    apiUrl: "https://api.kobonaut.com/feedback" // process.env.ELM_APP_API_URL
   }
 });
 
 app.ports.storeSettings.subscribe(function(data) {
-  localStorage.setItem('settings', JSON.stringify(data));
+  localStorage.setItem("settings", JSON.stringify(data));
 });
 
 registerServiceWorker();
